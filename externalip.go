c44aff94748cbbae8b2896f8ea97dfe2290e6eb0
Okke Timm <okketimm@me.com>
2015-04-27 13:12:53 +0200

    Remove unnecessary dependcy on github.com/jackpal/gateway

--- externalip.go
+++ externalip.go
@@ -1,22 +1,21 @@
 package main
 
 import natpmp "github.com/jackpal/go-nat-pmp"
-import gateway "github.com/jackpal/gateway"
 import "fmt"
 
 func main() {
-	gwAddr, err := gateway.DiscoverGateway()
+	client, err := natpmp.NewClientForDefaultGateway()
 	if err != nil {
-		// fmt.Print("Couldn't dertermine default gateway\n")
+		// fmt.Print("Couldn't determine default gateway\n")
 		return
 	}
 
-	client := natpmp.NewClient(gwAddr)
 	response, err := client.GetExternalAddress()
 	if err != nil {
-		// fmt.Printf("No answer from gateway %v\n", gatewayAddr)
+		// fmt.Print("No answer from gateway\n")
 		return
 	}
+
 	addr := response.ExternalIPAddress
 	fmt.Printf("%d.%d.%d.%d\n", addr[0], addr[1], addr[2], addr[3])
 }
